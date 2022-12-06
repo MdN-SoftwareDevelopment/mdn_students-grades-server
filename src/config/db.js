8bfad5ba9298082c14865e5a1fcf934b1b0e7a62
MorveN11 <morales.patty.jose@gmail.com>
2022-12-06 11:31:19 -0400

    Add DotEnv

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,9 @@
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_NAME = process.env.DB_NAME || 'student-grades';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_NAME = process.env.DB_NAME;
