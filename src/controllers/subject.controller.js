f915ca561985fb6c0792be7ca910d23d69268179
MayerliSantander <mayerli.santander.14418847@usalesiana.edu.bo>
2022-12-11 23:09:15 -0400

    Modify subject controller

--- src/controllers/subject.controller.js
+++ src/controllers/subject.controller.js
@@ -3,13 +3,13 @@ import pool from '../db.js';
 export const getSubjects = async (_req, res) => {
   try {
     const [results] = await pool.query(
-      'SELECT S.subject_name AS Materia, \
-      CONCAT(D.docent_name," " , D.docent_last_name) AS Docente, \
-      A.activity_name AS Actividad, A.grade AS Nota, A.feedback AS Feedback \
-      FROM activity AS A JOIN subject_U AS S ON A.id_subject = S.id_subject \
-      JOIN docents_subjects AS DS ON S.id_subject = DS.id_subject \
-      JOIN docent AS D ON DS.id_docent = D.id_docent \
-      WHERE S.subject_name = "Ingles";'
+      'SELECT S.subjectName AS SubjectU, \
+      CONCAT(D.docentName," " , D.docentLastName) AS Docent, \
+      A.activityName AS Activity, A.grade AS Grade, A.feedback AS Feedback\
+      FROM activity AS A JOIN subjectU AS S \
+      ON A.idSubject = S.idSubject JOIN docent AS D\
+      ON S.idDocent = D.idDocent\
+      WHERE S.subjectName = "Ingles I";'
     );
     res.json(results);
   } catch (error) {
